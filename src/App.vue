e1e5e3da7ed7722ba88889987e674441f52cadef
Ytenmei <ytm980582306@163.com>
2019-07-08 22:54:52 +0800

    配置REM适配

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>>
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'APP'
+  name: 'APP',
+  data () {
+    return {}
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
