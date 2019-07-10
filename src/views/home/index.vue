e4d8840b1519c1d8cc2e6feef8db28fe1f0c4beb
Ytenmei <ytm980582306@163.com>
2019-07-10 10:20:17 +0800

    封装API请求函数

---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Home Component</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIndex',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <van-nav-bar
+      title="标题"
+      left-arrow
+    />
+    <form>
+      <van-cell-group>
+        <van-field
+          v-model="user.mobile"
+          clearable
+          left-icon="phone-o"
+          placeholder="请输入手机号"
+        />
+        <van-field
+          v-model="user.code"
+          type="password"
+          left-icon="scan"
+          placeholder="请输入密码"
+        />
+      </van-cell-group>
+      <div class="login-btn">
+        <van-button class="btn" @click.prevent="handleLogin">登录</van-button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+import { login } from '@/api/user'
+
+export default {
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    async handleLogin () {
+      try {
+        const data = await login(this.user)
+        console.log(data)
+      } catch (err) {
+        console.log(err)
+        console.log('登录失败')
+      }
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.login-btn {
+  padding: 10px;
+  .btn {
+    width: 100%;
+    background-color:#6db4fb;
+    border-radius: 7px;
+  }
+}
+</style>
