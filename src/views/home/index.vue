a8063e97ba5632f5a2e6c51851a4ebf6c76befae
Ytenmei <ytm980582306@163.com>
2019-07-11 23:31:42 +0800

    首页-布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,44 @@
 <template>
   <div>
     <div class="home">
-      <van-nav-bar title="首页"></van-nav-bar>
+      <van-nav-bar title="首页"/>
+      <!--activeChannelIndex 绑定当前激活的标签页，使用索引
+       -->
+      <van-tabs v-model="activeChannelIndex">
+        <van-tab title="标签 1">
+          <!-- 下拉刷新
+            isLoading 用来控制下拉刷新的loading状态
+            下拉刷新的时候，它会自动将loading设置为true
+            @refresh 当下拉刷新的时候会触发
+           -->
+          <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+            <!--
+            列表组件：主要提供上拉加载更多功能
+            loading：控制加载中的效果
+            finished 从用来控制会否加载完毕
+            @load 加载更多的时候触发的一个事件，它自动会调用onLoad函数拿数据
+            以填充页面
+            它每次调用onLoad 中自动将loading设置为true
+            我们需要在onLoad中拿到本次加载的数据以后，将loading设置为false
+           -->
+            <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+          >
+            <van-cell
+              v-for="item in list"
+              :key="item"
+              :title="item"
+            />
+            </van-list>
+          </van-pull-refresh>
+        </van-tab>
+        <van-tab title="标签 2">内容 2</van-tab>
+        <van-tab title="标签 3">内容 3</van-tab>
+        <van-tab title="标签 4">内容 4</van-tab>
+      </van-tabs>
     </div>
   </div>
 </template>
@@ -11,6 +48,34 @@ export default {
   name: 'HomeIndex',
   data () {
     return {
+      activeChannelIndex: 0,
+      list: [],
+      loading: false,
+      finished: false,
+      isLoading: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    },
+    onRefresh () {
+      console.log('onRefresh')
+      setTimeout(() => {
+        this.isLoading = false
+      }, 3000)
     }
   }
 }
--- src/views/tabbar-layout/index.vue
+++ src/views/tabbar-layout/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-view/>
-    <van-tabbar v-model="active">
+    <van-tabbar route>
       <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
       <van-tabbar-item icon="search" to="/qa">问答</van-tabbar-item>
       <van-tabbar-item icon="friends-o" to="/video">视频</van-tabbar-item>
